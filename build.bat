983b231eff08ffdb7f1ea853e98a1452555eeb77
10277253 <114544114+10277253@users.noreply.github.com>
2022-09-28 09:22:07 -0600

    Added Myself
    Added Myself

---
+++ build.bat
@@ -0,0 +1,8 @@
+REM Comments ('remarks') in batch files start with REM.  Everything after REM is ignored.
+REM You may need to change the path of the COMPILER_LOC variable on your system
+
+set COMPILER_LOC="C:\Program Files\Java\jdk1.8.0_333\bin"
+
+%COMPILER_LOC%\javac githubpractice.java
+
+
---
+++ run.bat
@@ -0,0 +1,10 @@
+REM Comments ('remarks') in batch files start with REM.  Everything after REM is ignored.
+REM You may need to change the path of the JRE_LOC variable on your system
+
+set JRE_LOC="C:\Program Files\Java\jdk1.8.0_333\bin"
+
+%JRE_LOC%\java githubPractice
+
+REM If your JAVA_HOME environment variable is set correctly, then you do not need anything above.
+REM java TestInputValidation testData.txt
+
